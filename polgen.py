2390f27b651f492c3bbfe815fc6122f1f9438a38
p0l0satik <denis.cucu@yandex.ru>
2019-12-17 23:14:48 +0300

    more tests

--- polgen.py
+++ polgen.py
@@ -1,5 +1,5 @@
 size = (16,  32, 64, 128, 256, 512, 600, 700, 800, 900, 1024)
-cores = (2, 4, 8, 16)
+cores = (32, 64, 80)
 
 for test in range(3):
     print("mkdir test{0}".format(test))
@@ -9,5 +9,5 @@ for test in range(3):
         print("mpicc -std=gnu99 mpi{1}.c -o test{0}size{1}".format(test, cur_size))
         for core in cores:
             if (core <= cur_size):
-                print("mpisubmit.pl -p {2} -w 00:15  test{1}size{0} --stdout size{0}_n{2}_out --stderr n{2}_er_size{0}".format(cur_size,test,  core))
+                print("mpisubmit.pl -p {2} test{1}size{0} --stdout size{0}_n{2}_out --stderr n{2}_er_size{0}".format(cur_size,test,  core))
 
